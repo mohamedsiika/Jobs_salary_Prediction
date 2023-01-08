5291b12cbc4abbfb156e24df0a6730f42b07c55a
Mohamed Sayed <msiika70@gmail.com>
2023-01-08 14:42:46 +0200

    creating the endpoint

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,12 +1,28 @@
 import flask
 from flask import Flask, jsonify, request
+import pickle as pkl
+from data_input import data_in
+import numpy as np
 import json
+
+def load_models():
+    file_name="FlaskAPI/models/Model_file.p"
+    with open(file_name,'rb') as pickled:
+        model=pkl.load(pickled)
+    return model
 
 app = Flask(__name__)
 @app.route('/predict', methods=['GET'])
-
 def predict():
-    response = json.dumps({'response': 'yahhhh!'})
-    return response, 200
+    request_json=request.get_json()
+    x=request_json['input']
+    x_in=np.array(x).reshape(1,-1)
+
+    #import model
+    model = load_models()
+    prediction=model.predict(x_in)[0]
+    response=json.dumps({"response":prediction})
+    return (response,200)
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ FlaskAPI/request.py
@@ -0,0 +1,5 @@
+import requests
+from data_input import data_in
+
+url="http://127.0.0.1:5000"
+params={"input": data_in}
